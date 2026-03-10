12cc51bea050b4b17487bb97c748c8d5b34667a4
LeSamCham <samchamchedrackboti@gmail.com>
2026-03-10 13:03:48 +0100

    Implement solutions for exercises

--- exercise2/string_utils.py
+++ exercise2/string_utils.py
@@ -12,7 +12,8 @@ def reverse_string(s: str) -> str:
         The reversed string
     """
     # TODO: Implement this function
-    pass
+    n = len(s)
+    return s[n - 1 : : -1]
 
 
 def count_vowels(s: str) -> int:
@@ -27,7 +28,12 @@ def count_vowels(s: str) -> int:
         The number of vowels in the string
     """
     # TODO: Implement this function
-    pass
+    voy = ['a', 'e', 'i', 'o', 'u',]
+    nb_voy = 0
+    for l in s:
+        if l.lower() in voy:
+            nb_voy += 1
+    return nb_voy
 
 
 def is_palindrome(s: str) -> bool:
@@ -43,7 +49,8 @@ def is_palindrome(s: str) -> bool:
         True if the string is a palindrome, False otherwise
     """
     # TODO: Implement this function
-    pass
+    s_clean = s.lower().replace(" ", "")
+    return s_clean == reverse_string(s_clean)
 
 
 def capitalize_words(s: str) -> str:
@@ -57,4 +64,13 @@ def capitalize_words(s: str) -> str:
         The input string with the first letter of each word capitalized
     """
     # TODO: Implement this function
-    pass
+    if not s:  # Cas de la chaîne vide
+        return ""
+    n = len(s)
+    s0 = s[0].upper()
+    for i in range(1, n):
+        if s[i-1] == ' ':
+            s0 += s[i].upper()
+        else:
+            s0 += s[i]
+    return(s0)
