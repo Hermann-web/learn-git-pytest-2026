f69423cb238d6587c4860336531acce56b7b781f
ihsaneabdou07 <ihssaneabdou07@gmail.com>
2026-03-10 09:29:17 +0000

    test

--- exercise2/string_utils.py
+++ exercise2/string_utils.py
@@ -11,8 +11,11 @@ def reverse_string(s: str) -> str:
     Returns:
         The reversed string
     """
-    # TODO: Implement this function
-    pass
+    n=len(s)
+    C=[]
+    for i in range(0,n,1):
+        C.append(s[n-i])
+    return C
 
 
 def count_vowels(s: str) -> int:
@@ -26,8 +29,12 @@ def count_vowels(s: str) -> int:
     Returns:
         The number of vowels in the string
     """
-    # TODO: Implement this function
-    pass
+    n=len(s)
+    t=0
+    for j in range(0,n,1):
+        if s[j] is ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U" ]:
+            t=t+1
+    return t
 
 
 def is_palindrome(s: str) -> bool:
@@ -42,8 +49,15 @@ def is_palindrome(s: str) -> bool:
     Returns:
         True if the string is a palindrome, False otherwise
     """
-    # TODO: Implement this function
-    pass
+    C = s.lower().replace(" ", "")
+    n=len(C)
+    for i in range(0,n//2):
+        if  C[i]==C[n-1-i]:
+            print(f'{s} is palindrome')
+            return True
+        else:
+            print(f'{s} is not  palindrome')
+            return False
 
 
 def capitalize_words(s: str) -> str:
@@ -56,5 +70,9 @@ def capitalize_words(s: str) -> str:
     Returns:
         The input string with the first letter of each word capitalized
     """
-    # TODO: Implement this function
-    pass
+    L=list(s)
+    L[0]=L[0].upper()
+    for i in range(0, len(L)):
+        if L[i]==" ":
+            L[i]= L[i+1].upper()
+    return "".join(L)
