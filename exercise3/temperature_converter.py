df7c7da880bfbc1584b7a26ab1d37e8c5b74730a
RanyaAD <adiouaneranya2@gmail.com>
2026-03-11 13:44:15 +0000

    Explique ici ce que tu as fait (ex: correction du test X)

--- exercise3/temperature_converter.py
+++ exercise3/temperature_converter.py
@@ -18,7 +18,12 @@ def celsius_to_fahrenheit(celsius: Temperature) -> float:
         Temperature in Fahrenheit (rounded to 2 decimal places)
     """
     # TODO: Implement this function
-    pass
+    C=celsius 
+    F=C*(9/5)+32
+    return round(F,2)
+
+
+
 
 
 def fahrenheit_to_celsius(fahrenheit: Temperature) -> float:
@@ -34,7 +39,10 @@ def fahrenheit_to_celsius(fahrenheit: Temperature) -> float:
         Temperature in Celsius (rounded to 2 decimal places)
     """
     # TODO: Implement this function
-    pass
+    F=fahrenheit
+    C=(F-32)*(5/9)
+    return round(C,2)
+
 
 
 def celsius_to_kelvin(celsius: Temperature) -> float:
@@ -50,7 +58,9 @@ def celsius_to_kelvin(celsius: Temperature) -> float:
         Temperature in Kelvin (rounded to 2 decimal places)
     """
     # TODO: Implement this function
-    pass
+    C=celsius
+    return round(C+273.15,2)
+
 
 
 def kelvin_to_celsius(kelvin: Temperature) -> float:
@@ -69,4 +79,8 @@ def kelvin_to_celsius(kelvin: Temperature) -> float:
         ValueError: If kelvin is less than 0 (below absolute zero)
     """
     # TODO: Implement this function
-    pass
+    kelvin
+    if kelvin < 0:
+        raise ValueError("Temperature cannot be below absolute zero")
+    return round(kelvin - 273.15,2)
+        
