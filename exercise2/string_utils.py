b2a6e92658da787131a1b6680d487d951762ca08
Zhour23 <zhour.meskour@centrale-casablanca.ma>
2026-03-11 01:05:48 +0100

    Compléter les fonctions

--- exercise2/string_utils.py
+++ exercise2/string_utils.py
@@ -12,7 +12,7 @@ def reverse_string(s: str) -> str:
         The reversed string
     """
     # TODO: Implement this function
-    pass
+    return s[::-1]
 
 
 def count_vowels(s: str) -> int:
@@ -26,9 +26,9 @@ def count_vowels(s: str) -> int:
     Returns:
         The number of vowels in the string
     """
-    # TODO: Implement this function
-    pass
-
+    vowels = 'aeiou'
+    return sum(1 for char in s.lower() if char in vowels)
+ 
 
 def is_palindrome(s: str) -> bool:
     """
@@ -42,8 +42,8 @@ def is_palindrome(s: str) -> bool:
     Returns:
         True if the string is a palindrome, False otherwise
     """
-    # TODO: Implement this function
-    pass
+    cleaned = ''.join(c.lower() for c in s if c.isalnum())
+    return cleaned == cleaned[::-1]
 
 
 def capitalize_words(s: str) -> str:
@@ -56,5 +56,4 @@ def capitalize_words(s: str) -> str:
     Returns:
         The input string with the first letter of each word capitalized
     """
-    # TODO: Implement this function
-    pass
+    return s.title()
